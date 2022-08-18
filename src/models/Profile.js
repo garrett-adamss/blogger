f83ac8c3f021af3062b7d7d10949fa71343f227b
Garrett Adams <92003987+garrett-adamss@users.noreply.github.com>
2022-08-18 15:14:13 -0600

    profile model

---
+++ src/models/Profile.js
@@ -0,0 +1,10 @@
+export class Profile { 
+    constructor(){ 
+        this.id = data.id
+        this.name = data.name
+        this.bio = data.bio
+        this.coverImg = data.coverImg
+        this.picture = data.picture
+        this.email = data.email
+    }
+}
